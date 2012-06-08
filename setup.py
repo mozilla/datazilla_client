4fc2b3f73ac714fbd4fbfef207cba6bd8fe459ab
Malini Das <mdas@mozilla.com>
2012-06-08 18:17:57 -0400

    add setup.py support

---
+++ setup.py
@@ -0,0 +1,32 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import sys
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+deps = []
+
+try:
+    import json
+except ImportError:
+    deps.append('simplejson')
+
+setup(name='datazilla',
+      version=version,
+      description="Python library to interact with the datazilla server",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Malini Das',
+      author_email='mdas@mozilla.com',
+      url='https://github.com/mozilla/datazilla_client',
+      license='MPL',
+      packages=['datazilla'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=deps,
+      )
